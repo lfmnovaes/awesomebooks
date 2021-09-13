a2f229c9cf6004f6372d8c16199d07969dd29304
Luis <lfmnovaes@gmail.com>
2021-09-13 16:09:53 -0300

    Implement Add and Refresh function
    * Add listener to Add button to add and refresh the whole list
    * Fix logic (still not perfect)

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,49 @@
-let books = [];
-const Book = function (title,author) {
+const books = [];
+class Book {
+  constructor(title, author) {
     this.title = title;
     this.author = author;
+  }
+}
+
+const bookList = document.getElementById('booklist');
+const title = document.getElementById('enterTitle').value;
+const author = document.getElementById('enterAuthor').value;
+
+const addLi = (title, author) => {
+  const li = document.createElement('li');
+  li.innerText = `${title} - ${author}`;
+  const btn = document.createElement('button');
+  btn.type = 'button';
+  btn.innerText = 'Remove';
+  li.appendChild(btn);
+  return li;
 };
 
-const addBook = ()=>{
-    const title = document.getElementById('enterTitle').value;
-    const author = document.getElementById('enterAuthor').value;
-    console.log(title);
-    
-    books.push(new Book(title,author));
-    console.log(books);
-    console.log('This function is called');
+const addBook = () => {
+  bookList.appendChild(addLi(title, author));
+  
+  books.push(new Book(title,author));
+  console.log(books);
+  console.log('This function is called');
 };
-const removeBook = (title)=>{
-    books = books.filter(el => el.title != title);
+
+// const removeBook = (title)=>{
+//   books = books.filter(el => el.title != title);
+// };
+
+const addNRefresh = () => {
+  console.log('REFRESHING');
+  bookList.innerHTML = "";
+  books.push(new Book(title, author));
+  books.forEach(
+    (book) => {
+      bookList.appendChild(addLi(book.title, book.author));
+    },
+  );
 };
+
+document.getElementById("btnAdd").addEventListener("click", addNRefresh());
 
 //addBook('A','B');
 console.log(books);
