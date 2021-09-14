68b08c2e07866ed0b91cbc75e7a41aba3863cac8
alvarorf <aaruizf@unal.edu.co>
2021-09-14 14:44:56 -0500

    Refactor removeBook into a class method

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,5 @@
 let books = [];
 
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-
-  addBook() {
-    const title = document.getElementById('enterTitle').value;
-    const author = document.getElementById('enterAuthor').value;
-    // const storedBooks = JSON.parse(localStorage.books);
-    console.log('executing this')
-    books.push({title: title, author: author});
-    console.log(books);
-    // localStorage.setItem('books', JSON.stringify(storedBooks));
-    document.getElementById('enterTitle').value = '';
-    document.getElementById('enterAuthor').value = '';
-    refresh();
-  };
-
-}
-
 const bookList = document.getElementById('booklist');
 
 const addLi = (title, author) => {
@@ -34,17 +13,6 @@ const addLi = (title, author) => {
   return li;
 };
 
-const refresh = () => {
-  bookList.innerHTML = '';
-  console.log(localStorage.getItem('books'));
-  //const storedBooks = JSON.parse(localStorage.books);
-
-  console.log(books);
-
-  localStorage.setItem('books', JSON.stringify(books));
-  popLi();
-};
-
 const popLi = () => {
   books.forEach(
     (book) => {
@@ -53,37 +21,40 @@ const popLi = () => {
   );
 };
 
-
-/*
-const addBook = () => {
-  const title = document.getElementById('enterTitle').value;
-  const author = document.getElementById('enterAuthor').value;
-  const storedBooks = JSON.parse(localStorage.books);
-  storedBooks.push(new Book(title, author));
-  localStorage.setItem('books', JSON.stringify(storedBooks));
-  document.getElementById('enterTitle').value = '';
-  document.getElementById('enterAuthor').value = '';
-  refresh();
+const refresh = () => {
+  bookList.innerHTML = '';
+  localStorage.setItem('books', JSON.stringify(books));
+  popLi();
 };
-*/
 
-const removeBook = (e) => {
-  const tgt = e.target;
-  if (tgt.tagName === 'BUTTON') {
-    const title = e.composedPath()[1].firstChild.data.split(' - ')[0];
-    // let storedBooks = JSON.parse(localStorage.books);
-    // storedBooks = storedBooks.filter((el) => el.title !== title);
-    // localStorage.setItem('books', JSON.stringify(storedBooks));
-    //
-    // filter from books
-    //
-    tgt.closest('li').remove();
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-  refresh();
-};
 
-bookList.addEventListener('click', removeBook);
+  addBook() {
+    this.title = document.getElementById('enterTitle').value;
+    this.author = document.getElementById('enterAuthor').value;
+    books.push({ title: this.title, author: this.author });
+    document.getElementById('enterTitle').value = '';
+    document.getElementById('enterAuthor').value = '';
+    refresh();
+  }
+
+  removeBook(e) {
+    const tgt = e.target;
+    if (tgt.tagName === 'BUTTON') {
+      [this.title] = e.composedPath()[1].firstChild.data.split(' - ');
+      books = books.filter((el) => el.title !== this.title);
+      tgt.closest('li').remove();
+    }
+    refresh();
+  }
+}
+
 const myBook = new Book();
+bookList.addEventListener('click', myBook.removeBook);
 document.getElementById('btnAdd').addEventListener('click', myBook.addBook);
 
 window.onload = () => {
